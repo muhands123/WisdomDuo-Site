1336dd735420161546bc7b520310fe1b282901e4
muhands123 <muhand.slam.1221@gmail.com>
2025-08-07 02:53:38 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    FORM_ID: "1FAIpQLSd67IldHl6-unSyGjmGrAE4k1X7Q0b-jMFCQ8rPWz9TRY9B3g",
-    QUESTION_ID: "2071403553",
-    API_URL: "https://wisdomduo-api.example.com" // سيتم تحديثه لاحقاً
-};
-
-export { CONFIG };
